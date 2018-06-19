e80f94baa11c7c519e29fe8599f8337683dfcf72
Tomoyoshi <39577306+Yottan5571@users.noreply.github.com>
2018-06-19 23:41:52 +0000

    Add DESEncrypter

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace DESEncrypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.cbPaddingOption = new System.Windows.Forms.CheckBox();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.lbLog = new System.Windows.Forms.ListBox();
+            this.btnGetHash = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(40, 112);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "ENC";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(136, 112);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 1;
+            this.btnDecrypt.Text = "DEC";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // cbPaddingOption
+            // 
+            this.cbPaddingOption.AutoSize = true;
+            this.cbPaddingOption.Location = new System.Drawing.Point(82, 141);
+            this.cbPaddingOption.Name = "cbPaddingOption";
+            this.cbPaddingOption.Size = new System.Drawing.Size(101, 17);
+            this.cbPaddingOption.TabIndex = 2;
+            this.cbPaddingOption.Text = "Ignore Padding ";
+            this.cbPaddingOption.UseVisualStyleBackColor = true;
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(22, 68);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(535, 20);
+            this.tbPath.TabIndex = 3;
+            this.tbPath.Text = "Input file path";
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(563, 66);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(44, 23);
+            this.btnBrowser.TabIndex = 4;
+            this.btnBrowser.Text = "...";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // lbLog
+            // 
+            this.lbLog.FormattingEnabled = true;
+            this.lbLog.Location = new System.Drawing.Point(22, 162);
+            this.lbLog.Name = "lbLog";
+            this.lbLog.Size = new System.Drawing.Size(585, 147);
+            this.lbLog.TabIndex = 5;
+            // 
+            // btnGetHash
+            // 
+            this.btnGetHash.Location = new System.Drawing.Point(258, 112);
+            this.btnGetHash.Name = "btnGetHash";
+            this.btnGetHash.Size = new System.Drawing.Size(75, 23);
+            this.btnGetHash.TabIndex = 6;
+            this.btnGetHash.Text = "Get Hash";
+            this.btnGetHash.UseVisualStyleBackColor = true;
+            this.btnGetHash.Click += new System.EventHandler(this.btnGetHash_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 329);
+            this.Controls.Add(this.btnGetHash);
+            this.Controls.Add(this.lbLog);
+            this.Controls.Add(this.btnBrowser);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.cbPaddingOption);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "DES encrypter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.CheckBox cbPaddingOption;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.ListBox lbLog;
+        private System.Windows.Forms.Button btnGetHash;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+
+namespace DESEncrypter
+{
+    public partial class Form1 : Form
+    {
+        const string secretKey = "??lU&?=v";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            // Do encrypt
+
+            // Escape backup
+            File.Copy(tbPath.Text, tbPath.Text + ".back", true);
+
+            // For additional security Pin the key.
+            GCHandle gch = GCHandle.Alloc(secretKey, GCHandleType.Pinned);
+
+            byte[] bytearrayinput;
+
+            try
+            {
+                // Read file
+                using (FileStream fsEncrypted = new FileStream(tbPath.Text, FileMode.Open, FileAccess.Read))
+                {
+                    FileInfo size = new FileInfo(tbPath.Text); // Get file size
+                    bytearrayinput = new byte[size.Length];
+
+                    fsEncrypted.Read(bytearrayinput, 0, bytearrayinput.Length);
+
+                }
+
+                if (bytearrayinput.Length <= 0)
+                {
+                    return;
+                }
+
+                // Open output file
+                using (FileStream fsEncrypted = new FileStream(tbPath.Text, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    // Setup DES 
+                    DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+
+                    if (cbPaddingOption.Checked)
+                    {
+                        DES.Padding = PaddingMode.None; // Do not use padding in order to keep string hash
+                    }
+
+                    DES.Key = ASCIIEncoding.ASCII.GetBytes(secretKey);
+                    DES.IV = ASCIIEncoding.ASCII.GetBytes(secretKey);
+                    ICryptoTransform desencrypt = DES.CreateEncryptor();
+                    using (CryptoStream cryptostream = new CryptoStream(fsEncrypted, desencrypt, CryptoStreamMode.Write))
+                    {
+                        cryptostream.Write(bytearrayinput, 0, bytearrayinput.Length);
+                        cryptostream.Close();
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                lbLog.Items.Add(string.Format("Failed to encrypt. {0}", exp.Message));
+                gch.Free();
+                return;
+            }
+
+            gch.Free();
+            lbLog.Items.Add("Encrypt complete");
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            // Do decrypt
+            byte[] byteData;
+
+            // Setup DES 
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(secretKey);
+            DES.IV = ASCIIEncoding.ASCII.GetBytes(secretKey);
+
+
+            if (cbPaddingOption.Checked)
+            {
+                DES.Padding = PaddingMode.None; // Do not use padding in order to keep string hash
+            }
+
+            // For additional security Pin the key.
+            GCHandle gch = GCHandle.Alloc(secretKey, GCHandleType.Pinned);
+
+
+            try
+            {
+                // Open encrypted file, decrypt and store to byteData
+                using (FileStream fsRead = new FileStream(tbPath.Text, FileMode.OpenOrCreate, FileAccess.Read))
+                {
+                    FileInfo size = new FileInfo(tbPath.Text); // Get file size
+                    byteData = new byte[size.Length];
+
+                    ICryptoTransform encrypt = DES.CreateDecryptor();
+                    using (CryptoStream cryptostream = new CryptoStream(fsRead, encrypt, CryptoStreamMode.Read))
+                    {
+                        cryptostream.Read(byteData, 0, byteData.Length);
+                    }
+                }
+
+                // Create and Clear all contains
+                using (FileStream fsEnpty = new FileStream(tbPath.Text, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    fsEnpty.SetLength(0);
+                }
+
+                // Open and write plane text to output file
+                using (FileStream fsTempFile = new FileStream(tbPath.Text, FileMode.Open, FileAccess.Write))
+                {
+                    // Get byte data
+                    fsTempFile.Write(byteData, 0, byteData.Length);
+                }
+            }
+            catch(Exception exp)
+            {
+                lbLog.Items.Add(string.Format("Failed to decrypt. {0}", exp.Message));
+                gch.Free();
+                return;
+            }
+
+
+            gch.Free();
+
+            lbLog.Items.Add("Decrypt complete");
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            // Browse file find
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.FileName = tbPath.Text;
+
+            DialogResult ret = dialog.ShowDialog();
+            if (ret == DialogResult.OK)
+            {
+                tbPath.Text = dialog.FileName;
+            }
+        }
+
+        private void btnGetHash_Click(object sender, EventArgs e)
+        {
+            // Get hash code
+            using (StreamReader sr = new StreamReader(tbPath.Text))
+            {
+                char[] buff = new char[(int)sr.BaseStream.Length];
+                sr.Read(buff, 0, (int)sr.BaseStream.Length);
+                string ret = new string(buff);
+
+                lbLog.Items.Add(string.Format("Hash code = {0}", ret.GetHashCode()));
+            }
+
+        }
+    }
+}
